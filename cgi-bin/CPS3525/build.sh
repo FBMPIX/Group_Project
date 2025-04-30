c01378d0e28e3d6920323d829726e1739313c2a4
FBMPIX <165311459+FBMPIX@users.noreply.github.com>
2025-04-30 12:50:05 -0400

    Update build.sh

--- cgi-bin/CPS3525/build.sh
+++ cgi-bin/CPS3525/build.sh
@@ -1,2 +1,5 @@
-g++ -std=c++11 -o project3 *.cpp
-chmod 705 project3
+g++ -std=c++11 -Wall -c get_validate_input.cpp   
+g++ -std=c++11 -Wall -c style.cpp
+g++ -std=c++11 -Wall -c game_function.cpp
+g++ -std=c++11 -Wall -c main.cpp      
+g++ -o project3 main.o get_validate_input.o style.o game_function.o
